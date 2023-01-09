4a58250f1461da35845485aca5488d610eb08cb8
Anshu Yadav <115359930+Anshuy11@users.noreply.github.com>
2023-01-09 16:59:12 +0530

    "Styling footer section "

---
+++ src/components/Footer.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Footer = () => {
+    return <footer>
+
+    <div>
+    <h2>Wish on the Door</h2>
+    
+    </div>
+
+    <div>
+    <h5>Fallow Us</h5>
+    <div>
+    <a href='https://youtube.com' target={"blank"}>Youtube</a>
+    <a href='https://instagram.com' target={"blank"}>Instagram</a>
+    <a href='https://github.com/Anshuy11?tab=repositories' target={"blank"}>Github</a>
+    </div>
+    </div>
+
+    </footer>
+};
+export default Footer
