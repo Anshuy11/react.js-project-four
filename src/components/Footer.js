addd15ef44469309a64aabfb160969c1b90502d0
Anshu Yadav <115359930+Anshuy11@users.noreply.github.com>
2023-01-13 23:19:55 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -12,7 +12,7 @@ const Footer = () => {
     <h5>Fallow Us</h5>
     <div>
     <a href='https://youtube.com' target={"blank"}>Youtube</a>
-    <a href='https://instagram.com' target={"blank"}>Instagram</a>
+    <a href='https://vercel.com/anshuy115-gmailcom' target={"blank"}>Vercel</a>
     <a href='https://github.com/Anshuy11?tab=repositories' target={"blank"}>Github</a>
     </div>
     </div>
